78402ea0978b9bdce732eaaa28f24da153ea0740
niemingyan <1942350658@qq.com>
2021-11-22 00:12:01 +0800

    首页 新鲜好物ui完成

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,7 +1,8 @@
 import CSkeleton from './c-skeleton'
 import CCarousel from './c-carousel'
+import CMore from './c-more'
 
-const components = [CSkeleton, CCarousel]
+const components = [CSkeleton, CCarousel, CMore]
 
 export default {
   install (app) {
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,16 +4,21 @@
       <home-category></home-category>
       <home-banner></home-banner>
     </div>
+    <div>
+      <home-new></home-new>
+    </div>
   </div>
 </template>
 
 <script>
 import HomeCategory from '@/views/home/components/home-category'
 import HomeBanner from '@/views/home/components/home-banner'
+import HomeNew from '@/views/home/components/home-new'
 
 export default {
   name: 'home-page',
   components: {
+    HomeNew,
     HomeBanner,
     HomeCategory
   },
