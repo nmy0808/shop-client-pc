c2ccf8b043e8636fde91a22560e3490454f5e897
niemingyan <1942350658@qq.com>
2021-11-21 23:53:23 +0800

    首页请求整合完毕

--- src/api/home.js
+++ src/api/home.js
@@ -8,3 +8,42 @@ import request from '@/uitls/request'
 export function getHotBrandsApi (limit) {
   return request.get('/home/brand', { limit })
 }
+
+/**
+ * 首页-广告区域(pc-小程序)
+ * @returns {*}
+ */
+export function getBannersApi () {
+  return request.get('/home/banner')
+}
+
+/**
+ * 首页-新鲜好物
+ * @returns {*}
+ */
+export function getNewApi () {
+  return request.get('/home/new')
+}
+/**
+ * 首页-人气推荐
+ * @returns {*}
+ */
+export function getHotApi () {
+  return request.get('/home/hot')
+}
+
+/**
+ * 首页-产品区块
+ * @returns {*}
+ */
+export function getGoodsApi () {
+  return request.get('/home/goods')
+}
+
+/**
+ * 首页-最新专题
+ * @returns {*}
+ */
+export function getSpecialApi () {
+  return request.get('/home/special')
+}
