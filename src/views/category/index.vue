0dc37e191843be0ec61b55582f0806f4a8e09c07
niemingyan <1942350658@qq.com>
2021-11-24 00:28:59 +0800

    分类页面-二级分类和分类上篇ui完成

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class='category-page container'>
+  <div class='top-category container'>
     <c-bread>
       <c-bread-item :to='{name: "home"}'>首页</c-bread-item>
       <transition name='fade-right' mode='out-in'>
@@ -8,6 +8,29 @@
     </c-bread>
     <div class='category-banner'>
       <home-banner></home-banner>
+    </div>
+    <!-- 所有二级分类 -->
+    <div class='sub-list'>
+      <h3>全部分类</h3>
+      <ul>
+        <li v-for='i in 8' :key='i'>
+          <a href='javascript:;'>
+            <img src='http://zhoushugang.gitee.io/erabbit-client-pc-static/uploads/img/category%20(9).png'>
+            <p>空调</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- 不同分类商品 -->
+    <div class="ref-goods">
+      <div class="head">
+        <h3>- 海鲜 -</h3>
+        <p class="tag">温暖柔软，品质之选</p>
+        <CMore />
+      </div>
+      <div class="body">
+        <GoodsItem v-for="i in 5" :key="i" />
+      </div>
     </div>
   </div>
 </template>
@@ -19,11 +42,13 @@ import CBread from '@/components/library/c-bread'
 import CBreadItem from '@/components/library/c-bread-item'
 import { useStore } from 'vuex'
 import HomeBanner from '@/views/home/components/home-banner'
+import GoodsItem from '@/views/category/components/goods-item'
+import CMore from '@/components/library/c-more'
 
 export default {
   name: 'category-page',
   // eslint-disable-next-line vue/no-unused-components
-  components: { HomeBanner, CBreadItem, CBread },
+  components: { CMore, GoodsItem, HomeBanner, CBreadItem, CBread },
   setup() {
     const route = useRoute()
     const store = useStore()
@@ -42,9 +67,79 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang='less'>
 .category-banner {
   position: relative;
   height: 500px;
+}
+
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: @mainColor;
+          }
+        }
+      }
+    }
+  }
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+    .body {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+      padding: 0 65px 30px;
+    }
+  }
 }
 </style>
